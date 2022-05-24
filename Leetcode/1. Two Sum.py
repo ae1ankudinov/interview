30b1689e249f217f62d2e1a4d394749b0fb78050
ae1ankudinov <ae1ankudinov@gmail.com>
2022-05-24 18:58:16 +0300

    123

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -28,14 +28,32 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 '''
 
+# Time complexity - O(n^2)
+# Space complexity - O(1)
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: [int], target: int) -> [int]:
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
-if __name__ == '1. Two Sum':
-    s = Solution
-    nums1, target1 = [2, 7, 11, 15], 9
-    assert s.twoSum(nums1, target1) == [0, 1]
+# Time complexity - O(n)
+# Space complexity - O(n)
+class Solution:
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        nums_dict = {}
+        for num, val in enumerate(nums):
+            dif = target-val
+            if dif in nums_dict.keys():
+                return [nums_dict[dif], num]
+            else:
+                nums_dict[val] = num
+
+
+s = Solution()
+nums1, target1, out1 = [2, 7, 11, 15], 9, [0, 1]
+assert s.twoSum(nums1, target1) == out1
+nums2, target2, out2 = [3, 2, 4], 6, [1, 2]
+assert s.twoSum(nums2, target2) == out2
+nums3, target3, out3 = [3, 3], 6, [0, 1]
+assert s.twoSum(nums3, target3) == out3
